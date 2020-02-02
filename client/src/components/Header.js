d856a833e15c042733debfa0362a83acd8fc2de3
mindeby <debora.marisa.ribeiro@gmail.com>
2020-02-02 11:22:04 +0100

    header is now a stateless functional component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-export default class Header extends React.PureComponent {
-  render() {
-    const { context } = this.props;
-    const authUser = context.authenticatedUser;
-    
-//If the user is authenticated set the header to display the name of the user and a sign out button
-    return (
-      <div className="header">
-        <div className="bounds">
-          <h1 className="header--logo">Courses</h1>
-          <nav>
-            {authUser ?
-              <React.Fragment>
-                <span>Welcome, {authUser.firstName} {authUser.lastName}!</span>
-                <Link className="signout" to="/signout">Sign Out</Link>
-              </React.Fragment>
-            :
-              <React.Fragment>
-                <Link className="signup" to="/signup">Sign Up</Link>
-                <Link className="signin" to="/signin">Sign In</Link>
-              </React.Fragment>
-            }
-          </nav>
-        </div>
+export default (props) => {
+  return (
+    <div className="header">
+      <div className="bounds">
+        <h1 className="header--logo">Courses</h1>
+        <nav>
+          {props.context.authenticatedUser ?
+            <React.Fragment>
+              <span>Welcome, {props.context.authenticatedUser.firstName} {props.context.authenticatedUser.lastName}!</span>
+              <Link className="signout" to="/signout">Sign Out</Link>
+            </React.Fragment>
+          :
+            <React.Fragment>
+              <Link className="signup" to="/signup">Sign Up</Link>
+              <Link className="signin" to="/signin">Sign In</Link>
+            </React.Fragment>
+          }
+        </nav>
       </div>
-    );
-  }
-};
+    </div>
+  );
+}
